3e6b27839d3021f02a3ba57f5d3939f2d7f21054
Mark Anderson <udacity.com@ma7.org>
2015-08-28 11:40:10 -0500

    style clean up

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@
 
 // Enemies our player must avoid
 var Enemy = function () {
-    "use strict";
+    'use strict';
     var todo = 0;
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
@@ -20,7 +20,7 @@ var Enemy = function () {
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function (dt) {
-    "use strict";
+    'use strict';
     var todo = 0;
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
@@ -29,7 +29,7 @@ Enemy.prototype.update = function (dt) {
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function () {
-    "use strict";
+    'use strict';
     var todo = 0;
     //ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
@@ -39,7 +39,7 @@ Enemy.prototype.render = function () {
 // This class requires an update(), render() and
 // a handleInput() method.
 var Player = function () {
-    "use strict";
+    'use strict';
     this.x = 200;
     this.y = 400;
     this.hStep = 100; //horizontal step
@@ -48,42 +48,42 @@ var Player = function () {
 };
 
 Player.prototype.update = function (dt) {
-    "usestrict";
+    'use strict';
     var todo = 0;
 };
 
 Player.prototype.render = function (dt) {
-    "use strict";
+    'use strict';
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // TODO yike, lots of hardcoded number need to be gone
 Player.prototype.handleInput = function (key) {
-    "use strict";
-    var delta_x = 0;
-    var delta_y = 0;
+    'use strict';
+    var deltaX = 0;
+    var deltaY = 0;
     switch (key) {
     case 'left':
-        delta_x = -1;
+        deltaX = -1;
         break;
     case 'right':
-        delta_x = 1;
+        deltaX = 1;
         break;
     case 'up':
-        delta_y = -1;
+        deltaY = -1;
         break;
     case 'down':
-        delta_y = 1;
+        deltaY = 1;
         break;
         // no default action required
     }
-    this.x = Math.max(0, Math.min(400, this.x + this.hStep * delta_x));
-    this.y = Math.max(0, Math.min(400, this.y + this.vStep * delta_y));
+    this.x = Math.max(0, Math.min(400, this.x + this.hStep * deltaX));
+    this.y = Math.max(0, Math.min(400, this.y + this.vStep * deltaY));
     this.checkWin();
 };
 
 Player.prototype.checkWin = function () {
-    "use strict";
+    'use strict';
     if (this.y === 0) {
         this.y = 400;
         this.x = 200;
@@ -103,7 +103,7 @@ var player = new Player();
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function (e) {
-    "use strict";
+    'use strict';
     var allowedKeys = {
         '37': 'left',
         '38': 'up',
